4cd109404cf838222409af591c05807255a5c094
t3k3 <teke629@gmail.com>
2022-01-09 21:34:54 +0300

    edit Category model

--- models/Category.go
+++ models/Category.go
@@ -6,6 +6,6 @@ import (
 
 type Category struct {
 	ID            primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Category_name string             `json:"category_name" bson:"category_name,omitempty"`
-	Sub_category  uint               `json:"sub_category" bson:"sub_category,omitempty"`
+	Category_name string             `json:"category_name,omitempty" bson:"category_name,omitempty"`
+	Sub_category  uint               `json:"sub_category,omitempty" bson:"sub_category,omitempty"`
 }
